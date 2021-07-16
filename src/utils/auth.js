fcd974e779061bc297cb2e40fd2183ba769abcc9
Aleksandra Dolgushina <sashadolgushina@yandex.ru>
2021-07-16 16:28:08 +0300

    feat

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://mesto.dolgushina.nomoredomains.rocks';
+export const BASE_URL = 'https://api.mesto.dolgushina.nomoredomains.rocks';
 
 const checkResponse = (response) => response.ok ? response.json() : Promise.reject(`'Ошибка: ${response.status}'`)
 
